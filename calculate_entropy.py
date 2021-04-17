ce6edd5e64089d1b38e811deba20c62a9033d4da
k-z-m-r <jdkazime@buffalo.edu>
2021-04-17 04:41:31 -0400

    Added beta entropy model
    I added the beta (Community) entropy model and also some analysis on that.  I don't have the ability to copy a graph yet, so once I merge this into development I'll be able to.  I think this branch is done, though.

--- calculate_entropy.py
+++ calculate_entropy.py
@@ -1,4 +1,4 @@
-from numpy import isnan
+from numpy import exp, isnan, log
 from numpy.linalg import eigh
 from scipy.special import xlogy
 
@@ -13,7 +13,24 @@ def m_entropy(G):
     
     f = eigenvalues / (2*M)
     
-    H = -xlogy(f, f)
+    H = -xlogy(f, f)/log(2)
+    H[isnan(H)] = 0
+    
+    return H
+
+def b_entropy(G, beta = 1):
+    
+    '''
+        G -> the input graph.
+        beta -> a time-scale parameter
+    '''
+    
+    eigenvalues = G.eigenvalues
+    
+    f = exp(-beta*eigenvalues)
+    f /= f.sum()
+    
+    H = -xlogy(f, f)/log(2)
     H[isnan(H)] = 0
     
     return H
